b4dfa5f8da3860d924bd5fcb4c1e831f5a0882ff
Bhuvni Shah <bhuvni.shah@gmail.com>
2021-02-21 10:08:17 -0600

    final

--- server/src/app.py
+++ server/src/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("test.html")
+    return render_template("index.html")
 
 @app.route('/goals')
 def goals():
@@ -23,7 +23,7 @@ def goals():
 
 def workingHours(hour):
   send_to_db = hour
-  print(send_to_db)
+  return render_template("goals.html", time = send_to_db)
 
 @app.route('/goals', methods=['POST'])
 def my_form_post():
@@ -33,6 +33,10 @@ def my_form_post():
 @app.route('/buddies')
 def buddies():
     return render_template("buddies.html")
+
+@app.route('/buddies_personal')
+def buddiesPersonal():
+    return render_template("buddies_personal.html")
 
 @app.route('/check_in')
 def check_in():
@@ -44,54 +48,3 @@ def notific_log():
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-
-# from flask import Flask, render_template, request
-# # from pyowm import OWM
-# app = Flask(
-#     __name__,
-#     template_folder="templates"
-# )
-# app.static_folder = 'static'
-# # owm = OWM('1dcc5e56f0ad69d652fb00cf0b0c81dd')
-
-# @app.route('/')
-# def home():
-#     return render_template("test.html")
-
-# @app.route('/goals')
-# def goals():
-#     return render_template("goals.html")
-
-# def workingHours(hour):
-#   send_to_db = hour
-#   print(send_to_db)
-
-# @app.route('/goals', methods=['POST'])
-# def my_form_post():
-#     text = request.form['text']
-#     return workingHours(int (text))
-
-# @app.route('/buddies')
-# def buddies():
-#     return render_template("buddies.html")
-
-# @app.route('/check_in')
-# def check_in():
-#     return render_template("check_in.html")
-
-# @app.route('/notific_log')
-# def notific_log():
-#     return render_template("notific_log.html")
-
-# # @app.route('/weather/<city>')
-# # def weather(city):
-# #     return f'It\'s always sunny in {city}' 
-
-# # @app.route('/weather/<city>')
-# # def weather(city):
-# #     weather_manager = owm.weather_manager()
-# #     weather_at_place = weather_manager.weather_at_place(f'{city},US')
-# #     return weather_at_place.weather.temperature('celsius')
-
-# # Keep this at the bottom of run.py
-# app.run(debug=True)
